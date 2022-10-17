932dd97e4f4d96d45bb79932a7e3d2c92bcf3eef
chanet93 <ycdiaz1993@gmail.com>
2022-10-17 15:25:05 -0700

    add jpa annotation

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -1,25 +1,31 @@
 package com.example.demo.student;
 
 
+import jdk.jfr.DataAmount;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table
+@Data
 public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1)
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
     private String name;
     private String email;
     private LocalDate dob;
     private Integer age;
-
-    public Student(Long id, String name, String email, Integer age, LocalDate dob) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.age = age;
-        this.dob = dob;
-    }
-
-    public Student() {
-    }
 
     public Student(String name, String email, Integer age, LocalDate dob) {
         this.name = name;
@@ -28,54 +34,4 @@ public class Student {
         this.dob = dob;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public LocalDate getDob() {
-        return dob;
-    }
-
-    public void setDob(LocalDate dob) {
-        this.dob = dob;
-    }
-
-    @Override
-    public String toString() {
-        return "Student{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                ", age=" + age +
-                ", dob=" + dob +
-                '}';
-    }
 }
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -11,7 +11,7 @@ public class StudentService {
 
     public List<Student> getStudents(){
         return List.of(
-                new Student(1L, "Mariam","mariam@gmail.com",
+                new Student("Mariam","mariam@gmail.com",
                         21, LocalDate.of(2000, Month.JANUARY,5))
         );
     }
